18afdb70e4c5139fbf0e10820a73807a9e5ca560
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-08-07 21:51:43 -0300

    First field members of Tile class and constructor

--- domino-models/Tile.cs
+++ domino-models/Tile.cs
@@ -4,5 +4,21 @@ namespace domino_models
 {
     public class Tile
     {
+        private const int MAX_END_VALUE = 6;
+
+        private int _end1 = int.MinValue;
+
+        private int _end2 = int.MinValue;
+
+        public Tile (int end1, int end2)
+        {
+            if (end1 == maxEndValue || end2 == maxEndValue)
+            {
+                throw new InvalidOperationException($"Maximum end value is {MAX_END_VALUE}");
+            }
+            _end1 = end1;
+            _end2 = end2;
+        }
+
     }
 }
