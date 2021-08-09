fd3f578ff154378ed2b4a6f739316f6f9f0d631f
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-08-08 21:39:14 -0300

    Adding tests to Utils.cs

---
+++ domino-models-test/UtilsTest.cs
@@ -0,0 +1,41 @@
+using static System.Math;
+using Xunit;
+using FluentAssertions;
+using domino.models;
+using System;
+using static domino.models.Utils;
+
+namespace domino_models_test
+{
+  public class UtilsTest
+  {
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 6)]
+    [InlineData(4, 24)]
+    [InlineData(5, 120)]
+    public void FactorialTest(int n, int expected)
+    {
+      int result = factorial(n);
+      result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(2, 6)]
+    [InlineData(3, 10)]
+    [InlineData(4, 15)]
+    [InlineData(5, 21)]
+    [InlineData(6, 28)]
+    [InlineData(7, 36)]
+    [InlineData(8, 45)]
+    [InlineData(9, 55)]
+    [InlineData(10, 66)]
+    public void DominoSetSizeTest(int maxpips, int expected_size)
+    {
+      int result = DominoSetSize(maxpips);
+      result.Should().Be(expected_size);
+    }
+  }
+}
